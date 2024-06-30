c8bcebcd9177f292a3cb5867ba277e35e29f317c
Pawel Zurakowski <55536129+pzurakowski@users.noreply.github.com>
2024-06-30 21:37:54 +0200

    Add aerospike

---
+++ front/front/db.py
@@ -0,0 +1,78 @@
+import aerospike
+from front.models import UserProfile, UserTag
+import snappy
+
+class UserProfileDAO:
+    def __init__(self):
+        self.NAMESPACE = 'mimuw'
+        self.SET = 'user_profile'
+        self.RETRY_COUNT = 3
+
+        config = {
+            'hosts': [('st117vm106.rtb-lab.pl', 3000),
+                      ('st117vm107.rtb-lab.pl', 3000),
+                      ('st117vm108.rtb-lab.pl', 3000),
+                      ('st117vm109.rtb-lab.pl', 3000),
+                      ('st117vm110.rtb-lab.pl', 3000)]
+        }
+
+        self.client = aerospike.client(config).connect()
+    
+    def get(self, cookie: str) -> UserProfile:
+        profile, _, = self._create_or_get(cookie)
+        return profile
+
+    def _create_or_get(self, cookie: str) -> tuple[UserProfile, int]:
+        key = (self.NAMESPACE, self.SET, cookie)
+        try:
+            (_, meta, compressed) = self.client.get(key)
+            decompressed = snappy.decompress(compressed['data'])
+
+            user_profile = UserProfile.model_validate_json(decompressed)
+            generation = meta['gen']
+            
+        except aerospike.exception.RecordNotFound:
+            user_profile = UserProfile(
+                cookie=cookie,
+                views=[],
+                buys=[]
+            )
+            generation = 0
+            self._put(user_profile, generation)
+        
+        return user_profile, generation
+    
+    def _put(self, user_profile: UserProfile, generation: int) -> bool:
+        key = (self.NAMESPACE, self.SET, user_profile.cookie)
+        
+        json = user_profile.model_dump_json()
+        compressed = snappy.compress(json)
+
+        try:
+            self.client.put(key, {'data': compressed}, meta={'gen': generation}, policy={'gen': aerospike.POLICY_GEN_EQ})
+        except aerospike.exception.RecordGenerationError:
+            print("generation error")
+            return False
+        
+        return True
+        
+    def _add_tag(self, user_tag: UserTag) -> bool:
+        profile, generation = self._create_or_get(user_tag.cookie)
+
+        if user_tag.action == "VIEW":
+            profile.views.append(user_tag)
+        elif user_tag.action == "BUY":
+            profile.buys.append(user_tag)
+        
+        return self._put(profile, generation)
+
+    def add_tag(self, user_tag: UserTag):
+        for _ in range(self.RETRY_COUNT):
+            if self._add_tag(user_tag):
+                return
+
+        raise Exception('Failed to add tag')
+
+
+    def __del__(self):
+        self.client.close()
--- front/front/main.py
+++ front/front/main.py
@@ -1,63 +1,19 @@
 from fastapi import FastAPI, Query, Depends
-from pydantic import BaseModel
 from typing import Literal, List, Annotated, Union, Dict
-from functools import cache
 from datetime import datetime
+from front.models import UserTag, UserProfile, Aggregate
+from front.db import UserProfileDAO
 
 app = FastAPI()
 
-class ProductInfo(BaseModel):
-    product_id: int
-    brand_id: str
-    category_id: str
-    price: int
-
-class UserTag(BaseModel):
-    time: str
-    cookie: str
-    country: str
-    device: Literal["PC", "MOBILE", "TV"]
-    action: Literal["VIEW", "BUY"]
-    origin: str
-    product_info: ProductInfo
-
-class UserProfile(BaseModel):
-    cookie: str
-    views: List[UserTag]
-    buys: List[UserTag]
-
-class Aggregate(BaseModel):
-    columns: List[str]
-    rows: List[List[str]]
-
-@cache
-def profile_store():
-    return {}
-
 @app.post("/user_tags", status_code=204)
-async def user_tags(user_tag: UserTag, store: Annotated[Dict[str, UserProfile], Depends(profile_store)]):
+async def user_tags(user_tag: UserTag, dao: Annotated[Dict[str, UserProfile], Depends(UserProfileDAO)]):
     cookie = user_tag.cookie
-    
-    if cookie not in store:
-        store[cookie] = UserProfile(cookie=cookie, views=[], buys=[])
-
-    if user_tag.action == "VIEW":
-        store[cookie].views.append(user_tag)
-        if len(store[cookie].views) > 200:
-            store[cookie].views.pop(0)
-    elif user_tag.action == "BUY":
-        store[cookie].buys.append(user_tag)
-        if len(store[cookie].buys) > 200:
-            store[cookie].buys.pop(0)
-
+    dao.add_tag(user_tag)
 
 @app.post("/user_profiles/{cookie}", status_code=200)
-async def user_profiles(cookie: str, time_range: str, body: UserProfile, store: Annotated[Dict[str, UserProfile], Depends(profile_store)], limit: int = 200):
-    profile = store.get(cookie)
-
-    if profile is None:
-        print("No profile found")
-        return UserProfile(cookie=cookie, views=[], buys=[])
+async def user_profiles(cookie: str, time_range: str, body: UserProfile, dao: Annotated[Dict[str, UserProfile], Depends(UserProfileDAO)], limit: int = 200):
+    profile = dao.get(cookie)
 
     start_time_str, end_time_str = time_range.split('_')
     start_time = datetime.fromisoformat(start_time_str)
