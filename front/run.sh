56e54f3ab0b5da5d11e5e3013f9e386621cd40e6
Pawel Zurakowski <55536129+pzurakowski@users.noreply.github.com>
2024-07-02 14:03:43 +0200

    Change log level for front

--- front/run.sh
+++ front/run.sh
@@ -3,5 +3,5 @@
 if [ "$ENVIRONMENT" == "dev" ]; then
   uvicorn front.main:app --host 0.0.0.0 --port 8000 --reload
 else
-  uvicorn front.main:app --host 0.0.0.0 --port 8000
+  uvicorn front.main:app --host 0.0.0.0 --port 8000 --log-level error
 fi
